51e13f964b5ad409063cc59d6cebe7b602bac608
Ludde <ludde.llj@gmail.com>
2018-09-20 18:06:04 +0200

    It's burning

---
+++ A2/A2-MinimalAST/src/test/lang/ParseTests.java
@@ -0,0 +1,23 @@
+package lang;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class ParseTests {
+	/** Directory where the test input files are stored. */
+	private static final File TEST_DIRECTORY = new File("testfiles/parser");
+
+	@Test public void example() {
+		Util.testValidSyntax(TEST_DIRECTORY, "example.in");
+	}
+
+	@Test public void test1() {
+		Util.testValidSyntax(TEST_DIRECTORY, "test1.in")
+	}
+
+	@Test
+	public void error() {
+		Util.testSyntaxError(TEST_DIRECTORY, "error.in");
+	}
+}
